0e2cf06e83122e7ec2b7c36c7acfc62a34770497
simon04PL <szymon.msa@gmail.com>
2024-05-19 00:26:29 +0200

    18-05-2024 add few photo

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,9 +2,32 @@
 
 @section('content')
 <div class="container">
-    <div class="row">
-        <div class="logo_main">
-            <img src="https://placehold.co/150x150?text=Hello+World" />
+    <div class="profile-info_profile">
+        <div class="logo">
+            <div class="logo_main">
+                <img src="https://placehold.co/150x150?text=Hello+World" />
+            </div>
+        </div>
+        <div class="profile-info_details">
+            <h1>{{ Auth::user()->name }}</h1>
+            <div class="profile-info_account-details">
+                <p>id: <strong>{{ Auth::user()->id }}</strong></p>
+                <p>utworzony: <strong>{{ Auth::user()->created_at }}</strong></p>
+                <p>ostatnia zmiana: <strong>{{ Auth::user()->updated_at }}</strong></p>
+            </div>
+            <span class="profile-info_mail">{{ Auth::user()->email }}</span>
+        </div>
+    </div>
+
+    <div class="photo">
+        <div class="photo_listing">
+            <img src="https://placehold.co/400x400?text=Hello+World" />
+        </div>
+        <div class="photo_listing">
+            <img src="https://placehold.co/400x400?text=Hello+World" />
+        </div>
+        <div class="photo_listing">
+            <img src="https://placehold.co/400x400?text=Hello+World" />
         </div>
     </div>
 </div>
