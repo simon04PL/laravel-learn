8f059d0ff616f7e4bc3a257707f4b802dbdb3ed0
simon04PL <szymon.msa@gmail.com>
2024-05-18 23:20:32 +0200

    18-05-2024 create log in system

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,21 +2,9 @@
 
 @section('content')
 <div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
-
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    {{ __('You are logged in!') }}
-                </div>
-            </div>
+    <div class="row">
+        <div class="logo_main">
+            <img src="https://placehold.co/150x150?text=Hello+World" />
         </div>
     </div>
 </div>
