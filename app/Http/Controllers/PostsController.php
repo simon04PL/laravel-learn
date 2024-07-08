5596705744c5323f75c93d7659bf50e49e6a8303
simon04PL <szymon.msa@gmail.com>
2024-07-08 22:34:05 +0200

    08-07-2024 add save image directory to database

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -22,10 +22,13 @@ class PostsController extends Controller
             'image' => ['required', 'image'],
         ]);
 
-        dd(request('image')->store('uploads', 'public'));
+        $imagePath = request('image')->store('uploads', 'public');
 
-        auth()->user()->posts()->create($data);
+        auth()->user()->posts()->create([
+            'caption' => $data['caption'],
+            'image' => $imagePath,
+        ]);
 
-        dd(request()->all());
+        return redirect('/profile/' . auth()->user()->id);
     }
 }
